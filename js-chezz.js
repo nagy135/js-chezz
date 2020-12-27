c16fb4693a2baecc2c5c6c58c70ce15cd89a6afd
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-12-27 14:08:26 +0100

    feat: working "one step possible moves"

--- js-chezz.js
+++ js-chezz.js
@@ -3,6 +3,7 @@ const size = 8;
 const piece_size = 40;
 const piece_margin = square_size / 2 - piece_size / 2;
 const white =  'rgb(255,255,255)';
+const red =  'rgb(200,20,20)';
 const black =  'rgb(0,0,0)';
 
 class Player {
@@ -25,39 +26,43 @@ class Player {
         for (let pawn of this.pieces.pawns)
             pawn.draw(colorBit);
 
-        console.log(this.pieces.queen.available_moves());
+
+        this.ctx.fillStyle = red;
+        for(let move of this.pieces.queen.available_moves()){
+            this.ctx.fillRect(piece_margin + square_size * (this.pieces.queen.x + move[0]), piece_margin + square_size * (this.pieces.queen.y + move[1]), 30,30);
+        }
     }
-    add_enemy(enemy){
+    init_game(enemy){
         this.enemy = enemy;
         if (this.color == 'black'){
             this.pieces = {
-                king: new King(this.ctx, 4,0, enemy),
-                queen: new Queen(this.ctx, 3,0, enemy),
-                rookL: new Rook(this.ctx, 0,0, enemy),
-                rookR: new Rook(this.ctx, 7,0, enemy),
-                bishopL: new Bishop(this.ctx, 2,0, enemy),
-                bishopR: new Bishop(this.ctx, 5,0, enemy),
-                knightL: new Knight(this.ctx, 1,0, enemy),
-                knightR: new Knight(this.ctx, 6,0, enemy),
+                king: new King(this.ctx, 4,0, this, enemy),
+                queen: new Queen(this.ctx, 3,0, this, enemy),
+                rookL: new Rook(this.ctx, 0,0, this, enemy),
+                rookR: new Rook(this.ctx, 7,0, this, enemy),
+                bishopL: new Bishop(this.ctx, 2,0, this, enemy),
+                bishopR: new Bishop(this.ctx, 5,0, this, enemy),
+                knightL: new Knight(this.ctx, 1,0, this, enemy),
+                knightR: new Knight(this.ctx, 6,0, this, enemy),
                 pawns: []
             };
         } else {
             this.pieces = {
-                king: new King(this.ctx, 4,7, enemy),
-                queen: new Queen(this.ctx, 3,7, enemy),
-                rookL: new Rook(this.ctx, 0,7, enemy),
-                rookR: new Rook(this.ctx, 7,7, enemy),
-                bishopL: new Bishop(this.ctx, 2,7, enemy),
-                bishopR: new Bishop(this.ctx, 5,7, enemy),
-                knightL: new Knight(this.ctx, 1,7, enemy),
-                knightR: new Knight(this.ctx, 6,7, enemy),
+                king: new King(this.ctx, 4,7, this, enemy),
+                queen: new Queen(this.ctx, 3,7, this, enemy),
+                rookL: new Rook(this.ctx, 0,7, this, enemy),
+                rookR: new Rook(this.ctx, 7,7, this, enemy),
+                bishopL: new Bishop(this.ctx, 2,7, this, enemy),
+                bishopR: new Bishop(this.ctx, 5,7, this, enemy),
+                knightL: new Knight(this.ctx, 1,7, this, enemy),
+                knightR: new Knight(this.ctx, 6,7, this, enemy),
                 pawns: []
             };
         }
         const pawnY = (this.color == 'black') ? 1 : 6;
         const direction = (this.color == 'black') ? 'down' : 'up';
         for (var i = 0; i < size; i++)
-            this.pieces.pawns.push(new Pawn(this.ctx, i, pawnY, enemy, direction));
+            this.pieces.pawns.push(new Pawn(this.ctx, i, pawnY, this, enemy, direction));
     }
 }
 
@@ -88,8 +93,8 @@ class Board {
         this.player1 = new Player('black', this.ctx);
         this.player2 = new Player('white', this.ctx);
 
-        this.player1.add_enemy(this.player2);
-        this.player2.add_enemy(this.player1);
+        this.player1.init_game(this.player2);
+        this.player2.init_game(this.player1);
     }
     draw(){
         this.draw_board();
@@ -100,11 +105,13 @@ class Board {
 
 class Piece {
     repeats = false;
-    constructor(ctx, x, y, player){
+    constructor(ctx, x, y, player, enemy){
         this.ctx = ctx;
         this.x = x;
         this.y = y;
         this.player = player;
+        this.enemy = enemy;
+        this.active = false;
         this.label = "";
     }
     draw(color){
@@ -123,6 +130,20 @@ class Piece {
         };
         return result;
     }
+    own_reserved_positions(){
+        var owned_positions = [];
+        owned_positions.push([this.player.pieces.king.x, this.player.pieces.king.y]);
+        owned_positions.push([this.player.pieces.queen.x, this.player.pieces.queen.y]);
+        owned_positions.push([this.player.pieces.rookL.x, this.player.pieces.rookL.y]);
+        owned_positions.push([this.player.pieces.rookR.x, this.player.pieces.rookR.y]);
+        owned_positions.push([this.player.pieces.bishopL.x, this.player.pieces.bishopL.y]);
+        owned_positions.push([this.player.pieces.bishopR.x, this.player.pieces.bishopR.y]);
+        owned_positions.push([this.player.pieces.knightL.x, this.player.pieces.knightL.y]);
+        owned_positions.push([this.player.pieces.knightR.x, this.player.pieces.knightR.y]);
+        for (var pawn of this.player.pieces.pawns)
+            owned_positions.push([pawn.x, pawn.y]);
+        return owned_positions;
+    }
     available_move(move){
         const x = move[0];
         const y = move[1];
@@ -131,13 +152,24 @@ class Piece {
             return false;
         if (this.y + y < 0)
             return false;
-        if (this.x + x > size - 1)
+        if (this.x + x >= size)
             return false;
-        if (this.y + y < size - 1)
+        if (this.y + y >= size)
             return false;
         // }}}
-        console.log(Object.keys(this.player.pieces));
 
+        /// {{{ check owned pieces
+        var fits = true;
+        const owned_positions = this.own_reserved_positions();
+        for (let key in owned_positions){
+            const reserved = owned_positions[key];
+            console.log(this.x + x, this.y + y, ' ##### ', reserved);
+            if (this.x + x == reserved[0] && this.y + y == reserved[1])
+                fits = false;
+        };
+        if (!fits)
+            return false;
+        // }}}
 
         return true;
     }
@@ -150,7 +182,6 @@ class King extends Piece {
         [1,0],
         [1,-1],
         [0,1],
-        [0,0],
         [0,-1],
         [-1,-1],
         [-1,0],
@@ -166,7 +197,6 @@ class Queen extends Piece {
         [1,0],
         [1,-1],
         [0,1],
-        [0,0],
         [0,-1],
         [-1,-1],
         [-1,0],
@@ -188,7 +218,6 @@ class Bishop extends Piece {
     repeats = true;
     moves = [
         [1,1],
-        [0,0],
         [-1,1],
         [1,-1]
     ];
@@ -200,7 +229,6 @@ class Knight extends Piece {
         [-2,1],
         [2,1],
         [2,-1],
-
         [1,2],
         [1,-2],
         [-1,2],
@@ -209,8 +237,8 @@ class Knight extends Piece {
 }
 class Pawn extends Piece {
     label = "P";
-    constructor(ctx, x, y, direction){
-        super(ctx, x, y);
+    constructor(ctx, x, y, player, enemy, direction){
+        super(ctx, x, y, player, enemy);
         this.direction = direction;
         if (this.direction == 'up')
             this.moves = [
